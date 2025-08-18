5bb64561467cb8f92efeed8a26e795c34490fac7
Daniel Roa <>
2025-08-18 03:55:23 -0400

    Correccion de docker front

--- bookstore-api/mixins/db.mixin.js
+++ bookstore-api/mixins/db.mixin.js
@@ -1,25 +1,26 @@
+"use strict";
+
 const DbService = require("moleculer-db");
 const MongoAdapter = require("moleculer-db-adapter-mongo");
 
-module.exports = function(collection) {
-	const schema = {
-		mixins: [DbService],
-		adapter: new MongoAdapter("mongodb://127.0.0.1:27017/bookstore_db", {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		}),
-		collection,
-		
-		methods: {
-			async seedDB() {
-				this.logger.info(`Seeding ${collection} collection...`);
-			}
-		},
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb://admin:admin123@mongodb:27017/bookstore_db?authSource=admin";
 
-		async afterConnected() {
-			this.logger.info(`Connected to ${collection} collection`);
-		}
-	};
+module.exports = function (collection) {
+  return {
+    mixins: [DbService],
+    adapter: new MongoAdapter(MONGO_URI),
+    collection,
 
-	return schema;
+    methods: {
+      async seedDB() {
+        this.logger.info(`Seeding ${collection} collection...`);
+      }
+    },
+
+    afterConnected() {
+      this.logger.info(`Connected to ${collection} collection`);
+    }
+  };
 };
